70498c3840ad6800265b5d409d607b7b5a95ded3
Adam Wang <934399003@qq.com>
2018-12-05 11:10:01 +0800

    RemoveDuplicatesfromSortedArray

---
+++ simple/26RemoveDuplicatesfromSortedArray.c
@@ -0,0 +1,21 @@
+
+void pullNums(int* nums, int start, int end){
+    for(int i = start; i<end; i++){
+        nums[i] = nums[i+1];
+    }
+}
+
+
+int removeDuplicates(int* nums, int numsSize) {
+    int newSize = numsSize;
+    int i = 0;
+    while(i < newSize - 1){
+        if (nums[i] == nums[i+1]){
+            pullNums(nums, i, newSize - 1);
+            newSize--;
+        } else{
+            i++;
+        }
+    }
+    return newSize;
+}
