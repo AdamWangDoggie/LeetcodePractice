5614aa0dba99f38244b0c869961ac7a30a0e6ce5
Adam Wang <934399003@qq.com>
2018-12-05 11:13:13 +0800

    RemoveElement

---
+++ simple/27RemoveElement.c
@@ -0,0 +1,19 @@
+void pullNums(int* nums, int start, int end){
+    for(int i = start; i<end; i++){
+        nums[i] = nums[i+1];
+    }
+}
+
+
+int removeElement(int* nums, int numsSize, int val) {
+    int i = 0, newSize = numsSize;
+    while(i < newSize){
+        if(nums[i]==val){
+            pullNums(nums, i, newSize - 1);
+            newSize--;
+        } else{
+            i++;
+        }
+    }
+    return newSize;
+}
