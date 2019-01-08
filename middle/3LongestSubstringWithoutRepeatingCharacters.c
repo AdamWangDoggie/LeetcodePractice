30a0d114b4f93e3561f46615d3eaf38de473d9b2
Adam Wang <934399003@qq.com>
2019-01-08 21:09:24 +0800

    3 opt

--- middle/3LongestSubstringWithoutRepeatingCharacters.c
+++ middle/3LongestSubstringWithoutRepeatingCharacters.c
@@ -3,27 +3,49 @@
 #include <memory.h>
 #include <stdio.h>
 
+
+
 // 只需要维护start的位置就好了, start->index是子串的范围, 
+// int lengthOfLongestSubstring(char* s) {
+//     int exists[256];
+//     memset(exists, -1, 256*sizeof(int));
+//     int index=0, start=0, ret=0;
+//     while (s[index] != '\0'){
+//         char ord_c = s[index];
+//         if(exists[ord_c] < start){
+//             exists[ord_c] = index;  // 记下这个字符的索引
+//             if(index-start + 1> ret){
+//                 ret = index-start + 1;
+//             }
+//             index++;
+//         }
+//         else {
+//             start = exists[ord_c] + 1;
+//         }
+        
+//     }
+//     return ret;
+// }
+
+int max(int a, int b){
+    if(a > b){return a;}else{return b;}
+}
+
 int lengthOfLongestSubstring(char* s) {
     int exists[256];
     memset(exists, -1, 256*sizeof(int));
-    int index=0, start=0, ret=0;
+
+    int index=0, left=-1, ret=0;
     while (s[index] != '\0'){
         char ord_c = s[index];
-        if(exists[ord_c] < 0 || exists[ord_c] < start){
-            exists[ord_c] = index;  // 记下这个字符的索引
-            if(index-start + 1> ret){
-                ret = index-start + 1;
-            }
-            index++;
-        }
-        else {
-            start = exists[ord_c] + 1;
-        }
-        
+        left = max(left, exists[ord_c]);
+        exists[ord_c] = index;
+        ret = max(index-left, ret);
+        index++;
     }
     return ret;
 }
+
 
 int main(){
     int len = lengthOfLongestSubstring("abcabcabb");
